e7d40aac5d04ec6dc192132e4dd354771b8a9076
liangshiquan <liangshiquan@baidu.com>
2016-11-30 22:01:49 +0800

    add the table

--- container/review.js
+++ container/review.js
@@ -1,11 +1,78 @@
 import React, {Component} from 'react';
-import {Breadcrumb, Tabs} from 'antd';
+import {Breadcrumb, Tabs,Table} from 'antd';
+import reqwest from 'reqwest';
 
 const TabPane = Tabs.TabPane;
+
+const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    sorter: true,
+    render: name => `${name.first} ${name.last}`,
+    width: '20%',
+}, {
+    title: 'Gender',
+    dataIndex: 'gender',
+    filters: [
+        { text: 'Male', value: 'male' },
+        { text: 'Female', value: 'female' },
+    ],
+    width: '20%',
+}, {
+    title: 'Email',
+    dataIndex: 'email',
+}];
 
 export default class Review extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: [],
+            pagination: {},
+            loading: false
+        };
+    }
+
+    handleTableChange(pagination, filters, sorter) {
+        const pager = this.state.pagination;
+        pager.current = pagination.current;
+        this.setState({
+            pagination: pager,
+        });
+        this.fetch({
+            results: pagination.pageSize,
+            page: pagination.current,
+            sortField: sorter.field,
+            sortOrder: sorter.order,
+            ...filters,
+        });
+    }
+    fetch(params = {}) {
+        console.log('params:', params);
+        this.setState({ loading: true });
+        reqwest({
+            url: 'https://randomuser.me/api',
+            method: 'get',
+            data: {
+                results: 10,
+                ...params,
+            },
+            type: 'json',
+        }).then((data) => {
+            const pagination = this.state.pagination;
+            // Read total count from server
+            // pagination.total = data.totalCount;
+            pagination.total = 200;
+            this.setState({
+                loading: false,
+                data: data.results,
+                pagination,
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.fetch();
     }
     
     render() {
@@ -18,12 +85,21 @@ export default class Review extends Component {
                 </div>
                 <div className="ant-layout-container">
                     <div className="ant-layout-content">
-                        <div style={{ height: 700 }}>
+                        <div>
                             <Tabs type="card">
                                 <TabPane tab="未审核" key="1">
-                                    
+                                    <Table columns={columns}
+                                           rowKey={record => record.registered}
+                                           dataSource={this.state.data}
+                                           pagination={this.state.pagination}
+                                           loading={this.state.loading}
+                                           onChange={this.handleTableChange}
+                                    />
                                 </TabPane>
                                 <TabPane tab="审核通过" key="2">
+
+                                </TabPane>
+                                <TabPane tab="审核未通过" key="3">
 
                                 </TabPane>
                             </Tabs>
